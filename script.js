7beb54bf039ce5c56383bfcfaa1d18a454e4c55c
Mateus Leite <mateus.lcsilva@outlook.com>
2022-12-26 18:48:46 -0300

    melhorado o input de telefone
    o input agora retira todos os caracteres que não sejam números, para deixar com que a pessoa digite com os padrões desejados. acrescido também feedback imediato se o telefone é inválido

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const updateHref  = (phoneNumber) => {
+	document.querySelector('#sendMessage').href=`https://wa.me/${phoneNumber}/`
+}
+
 const getNumber = (element) => {
-  document.querySelector(
-    "#sendMessage"
-  ).href = `https://wa.me/55${element.value}/`;
-};
+	let phoneNumber = `55${element.value.replace(/[^0-9]/g, '')}`
+	updateHref(phoneNumber)
+
+	if(element.value.replace(/[^0-9]/g, '').length != 11){
+	document.querySelector('#sendMessage').classList.add('disabled')
+	element.style.borderColor = 'red'
+	document.querySelector('.alert-invalid-phone-number').style.display = 'block'
+	} else{
+	document.querySelector('#sendMessage').classList.remove('disabled')
+	element.style.borderColor = '#191919'
+	document.querySelector('.alert-invalid-phone-number').style.display = 'none'
+	}
+}
